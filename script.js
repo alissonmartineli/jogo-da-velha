78eb4e18eb87fa7aa0f1501cc3e3fc9a06e5e6d1
Alisson Martineli <martineli.alisson@gmail.com>
2021-02-15 16:48:39 -0300

    add function changePlayer

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ for (const place of places) {
     place.addEventListener('click', () => mark(place))
 }
 
+let player = 'X';
+
 function mark (place) {
     if (place.innerHTML !== "") {
         return;
     }
 
-    place.innerHTML = 'X';
+    place.innerHTML = player;
+
+    changePlayer();
+}
+
+function changePlayer() {
+    player = player === 'X' ? 'O' : 'X';
 }
