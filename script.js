4f951808a7c91a25d1f9e0f5a0fa7f4e52b08cdd
Alisson Martineli <martineli.alisson@gmail.com>
2021-02-15 17:54:08 -0300

    add function checkWin

--- script.js
+++ script.js
@@ -1,21 +1,68 @@
-const places = document.getElementsByClassName('col');
+let player = 'X';
+let winner = '';
+const places = {};
+const elements = document.getElementsByClassName('col');
+const message = document.getElementById('message');
 
-for (const place of places) {
-    place.addEventListener('click', () => mark(place))
+for (const el of elements) {
+    el.addEventListener('click', () => mark(el));
+    places[el.dataset.position] = el;
 }
 
-let player = 'X';
-
-function mark (place) {
-    if (place.innerHTML !== "") {
+function mark (el) {
+    if (winner !== '') {
         return;
     }
 
-    place.innerHTML = player;
+    if (el.innerHTML !== "") {
+        return;
+    }
+
+    el.innerHTML = player;
+
+    if (checkWin()) {
+        winner = player;
+        message.innerHTML = `Player ${player} win!`;
+        return;
+    }
 
     changePlayer();
 }
 
 function changePlayer() {
     player = player === 'X' ? 'O' : 'X';
+}
+
+function checkWin() {
+    const cases = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7],
+    ];
+
+    for (const caseTest of cases) {
+        [positionA , positionB, positionC] = caseTest;
+
+        if (places[positionA].innerHTML === "" || places[positionB].innerHTML === "" || places[positionC].innerHTML === "") {
+            continue;
+        }
+
+        if (
+            places[positionA].innerHTML === places[positionB].innerHTML &&
+            places[positionA].innerHTML === places[positionC].innerHTML &&
+            places[positionB].innerHTML === places[positionC].innerHTML
+        ) {
+            places[positionA].style.background = '#8bc34a';
+            places[positionB].style.background = '#8bc34a';
+            places[positionC].style.background = '#8bc34a';
+            return true;
+        }
+    }
+
+    return false;
 }
